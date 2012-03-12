d841ab10f0c0625facfd974998b6357a57881296
ephsu <ephsu@shrapnel.ics.uci.edu>
2012-03-12 11:30:40 -0700

    implemented Merge

--- src/mergingTwoArrays.java
+++ src/mergingTwoArrays.java
@@ -7,7 +7,7 @@ public class mergingTwoArrays {
 	 * enough buffer at the end to hold B. Complete the function
 	 * 'merge' to merge B into A in sorted order.
 	 */
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] a = new int[7];
@@ -15,18 +15,41 @@ public class mergingTwoArrays {
 		a[1] = 4;
 		a[2] = 6;
 		a[3] = 8;
-		
+
 		int[] b = {1, 3, 5};
-		
+
 		merge(a, b, 4, 3); //the result should be [1, 2, 3, 4, 5, 6, 8];
 	}
-	
+
 	public static void merge(int[] a, int[] b, int lastA, int lastB){
 		int indexA = lastA - 1; //index of last element in a
 		int indexB = lastB - 1; //index of last element in b
 		int indexMerged = lastB + lastA - 1;
-		
+
 		/* merge a and b */
+
+		for(int i = 0; i < lastB; i++){
+			a[i + lastA] = b[i];			
+		}
+
+		int smallest = a[0];
+		int smallestIndex = 0;
+		for(int i = 0; i < indexMerged; i++){
+			for(int j = i; j < indexMerged; j++){
+				if(smallest < a[j]){
+					smallest = a[j];
+					smallestIndex = j;
+				}
+			}
+			int temp = a[smallestIndex];
+			int a[smallestIndex] = a[i];
+			int a[i] = smallest;
+		}
+
+
+
+
+
 	}
 
 }
